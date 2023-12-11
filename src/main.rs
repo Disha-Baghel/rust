f2406bbf7d1e16c0ba5576851fbdce80c2509323
disha-baghel <bagheldisha708@gmail.com>
2023-12-11 15:35:38 +0530

    new

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::io;
 
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
     let mut input = String::new();
 
     io::stdin().read_line(&mut input).expect("Failed to read line");
     println!("You typed: {}", input);
+    
 }
  
