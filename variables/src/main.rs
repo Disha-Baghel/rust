fdcca97432fa0e165b6706922bf6a702a4d55a3f
disha-baghel <bagheldisha708@gmail.com>
2024-01-28 22:58:42 +0530

    practice

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -5,7 +5,18 @@ fn main() {
     println!("The value of x is: {x}");
     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
     println!("The value of THREE_HOURS_IN_SECONDS is: {THREE_HOURS_IN_SECONDS}");
-    let mut spaces = "      ";
-    spaces = spaces.len();
-    println!("The value of spaces.len() is: {spaces.len()}");
+    // let mut spaces = "      ";
+    // spaces = spaces.len();
+    // println!("The value of spaces.len() is: {spaces.len()}");
+
+    let tup : (i32, f32, u8)= (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("The value of y is : {y}");
+
+    let plus = plus_one(5);
+    println!("The value of plus is : {plus}");
+}
+
+fn plus_one(x: i32) -> i32 {
+    x+1
 }
