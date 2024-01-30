7f08944f004be1d166d130b42b07062e58b77e3f
disha-baghel <bagheldisha708@gmail.com>
2024-01-30 23:05:13 +0530

    used BufReader for optimization

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,5 +1,7 @@
 use clap::Parser;
 use std::io::BufReader; 
+use std::io::BufRead;
+// use std::fs::File;
 
 // Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
@@ -8,19 +10,21 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> std::io::Result<()>{
     // let pattern = std::env::args().nth(1).expect("no pattern given");
     // let path = std::env::args().nth(2).expect("no path given");
 
     let args = Cli::parse();
 
-    // println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
+    let file = std::fs::File::open(&args.path)?;
+    let reader = BufReader::new(file);
 
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
-    
-    for line in content.lines() {
+    for line in reader.lines() {
+        let line = line?;
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
     }
+    Ok(())
 }
+    
